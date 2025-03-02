7c3774ce9c1e97967bd214ddd2e4dc86d0af016f
JonesAlwyn <113624596+JonesAlwyn@users.noreply.github.com>
2025-03-02 10:40:29 +0530

    Add files via upload
    Day1

---
+++ dayone/Chrome.java
@@ -0,0 +1,23 @@
+package week1.dayone;
+
+class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       float versionis=91.0f;
+    	String developeris="Google";
+    	boolean isbetatype=false;
+    	int releaseyear=2008;
+    	char shortcutkey='c';
+    	
+    	System.out.println("version of the application="+versionis);
+    	System.out.println(" application Developer is="+developeris);
+    	System.out.println("Is it beta type="+isbetatype);
+    	System.out.println("The date of release="+2008);
+    	System.out.println("Shortcut key used in app=  "+shortcutkey);
+  
+    	
+    	
+	}
+
+} 
---
+++ dayone/HelloTestLeaf.java
@@ -0,0 +1,10 @@
+package week1.dayone;
+
+class HelloTestLeaf {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Welcome to testleaf");
+	}
+
+}
