b43786faba1870037fce000fcb2a8c4f4ca2904a
JonesAlwyn <113624596+JonesAlwyn@users.noreply.github.com>
2025-03-29 21:58:56 +0530

    Add files via upload
    Day-1

---
+++ day1/Chrome.java
@@ -0,0 +1,31 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		float versionOfType = 91.0f;
+		
+		String developerOfType = "Google";
+		
+		boolean isBeta = false;
+		
+		int releaseYear = 2008;
+		
+		char shortcutKey = 'C';
+		
+		System.out.println("Version Of Type="+versionOfType);
+		
+		System.out.println("developer Of Type="+developerOfType);
+		
+		System.out.println("is Beta="+isBeta);
+		
+		System.out.println("release Year="+releaseYear);
+		
+		System.out.println("Shortcut Key="+shortcutKey);
+				
+				
+		
+		
+	}
+
+}
